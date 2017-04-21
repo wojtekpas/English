d94796266d1e2e5bf25e89ecddf07a8d22b12173
wojtekpas <wojtek.pas@o2.pl>
2017-04-21 23:40:20 +0200

    adding script - renamer

---
+++ english_via_skype/scripts/renamer.py
@@ -0,0 +1,15 @@
+import os, time
+from shutil import copyfile
+path = "D:\English\english_via_skype\scripts"
+print ("Pliki w folderze: ")
+filenames = os.listdir(path)
+print (filenames)
+id = 49
+for filename in filenames:
+    names = str(filename).split(".docx")
+    if len(names) == 2:
+        oldname = names[0]
+        newname_orig = "lesson_%d_%s_original.docx" % (id, oldname)
+        newname_edit = "lesson_%d_%s_edit.docx" % (id, oldname)
+        copyfile(filename, newname_orig)
+        copyfile(filename, newname_edit)

e6472a16d6856ae68ed419cc50e48c6e8113918f
wojtek.pas <wojtek.pas@o2.pl>
2017-06-24 09:36:29 +0200

    before lesson 67

--- english_via_skype/scripts/renamer.py
+++ english_via_skype/scripts/renamer.py
@@ -4,7 +4,7 @@ path = "D:\English\english_via_skype\scripts"
 print ("Pliki w folderze: ")
 filenames = os.listdir(path)
 print (filenames)
-id = 65
+id = 67
 
 for filename in filenames:
     names = str(filename).split(".docx")

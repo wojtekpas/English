38128912bd4eb22abffff682ffab61e0df1e9f64
wojtek.pas <wojtek.pas@o2.pl>
2017-10-30 01:07:52 +0100

    lesson 103 - homework

--- english_via_skype/scripts/renamer.py
+++ english_via_skype/scripts/renamer.py
@@ -4,7 +4,7 @@ path = "D:\English\english_via_skype\scripts"
 print ("Pliki w folderze: ")
 filenames = os.listdir(path)
 print (filenames)
-id = 101
+id = 104
 
 for filename in filenames:
     names = str(filename).split(".docx")

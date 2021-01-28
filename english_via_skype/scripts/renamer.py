ac90de0e37a0692e50cb26c41680b4fa738f1649
Wojciech Pasternak <wojciech.pasternak@intel.com>
2021-01-28 10:09:56 +0100

    lesson 390 - Thursday

--- english_via_skype/scripts/renamer.py
+++ english_via_skype/scripts/renamer.py
@@ -10,7 +10,7 @@ path_vocab = "%s\vocab" % path
 print ("Pliki w folderze: ")
 filenames = os.listdir(path_script)
 print (filenames)
-id = 388
+id = 391
 
 for filename in filenames:
     if "py" in filename:

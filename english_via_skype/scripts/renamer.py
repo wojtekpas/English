a77856ed7008d3e09e6731e86deeb71bc55a4d6b
wojtekpas <wojtek.pas@o2.pl>
2021-07-19 09:02:20 +0200

    lesson 436 - Monday - replacement with Agnieszka

--- english_via_skype/scripts/renamer.py
+++ english_via_skype/scripts/renamer.py
@@ -10,7 +10,7 @@ path_vocab = "%s\vocab" % path
 print ("Pliki w folderze: ")
 filenames = os.listdir(path_script)
 print (filenames)
-id = 434
+id = 436
 
 for filename in filenames:
     if "py" in filename:

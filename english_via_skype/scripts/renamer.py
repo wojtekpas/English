e81fafa97d27803ec5d78809f63b10777c7bfa2b
wojtekpas <wojtek.pas@o2.pl>
2021-10-30 14:52:41 +0200

    lesson 461 - homework

--- english_via_skype/scripts/renamer.py
+++ english_via_skype/scripts/renamer.py
@@ -10,7 +10,7 @@ path_vocab = "%s\vocab" % path
 print ("Pliki w folderze: ")
 filenames = os.listdir(path_script)
 print (filenames)
-id = 460
+id = 462
 
 for filename in filenames:
     if "py" in filename:

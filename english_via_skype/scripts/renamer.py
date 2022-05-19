57976f67cd99cb41076ffc9a170fd5ba8ad600c9
wojtekpas <wojtek.pas@o2.pl>
2022-05-19 10:08:51 +0200

    lesson 511 - Thursday

--- english_via_skype/scripts/renamer.py
+++ english_via_skype/scripts/renamer.py
@@ -10,7 +10,7 @@ path_vocab = "%s\vocab" % path
 print ("Pliki w folderze: ")
 filenames = os.listdir(path_script)
 print(filenames)
-id = 510
+id = 511
 
 for filename in filenames:
     if "py" in filename:

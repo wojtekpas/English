0700d7512ce4ec9c602636f225cc2ea5d6b30ac8
wojtekpas <wojtek.pas@o2.pl>
2023-11-02 00:09:08 +0100

    lesson 610 - vocab and homework

--- english_via_skype/scripts/renamer.py
+++ english_via_skype/scripts/renamer.py
@@ -10,7 +10,7 @@ path_vocab = "%s\vocab" % path
 print ("Pliki w folderze: ")
 filenames = os.listdir(path_script)
 print(filenames)
-id = 610
+id = 611
 
 for filename in filenames:
     if "py" in filename:
